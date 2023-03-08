920c878f4c16bcd34502ad2598ce8757a104d593
VighneshPath <pathrikarvighnesh@gmail.com>
2023-03-08 10:41:46 +0530

    Refactor CalculatorFactory code into Calculators itself

--- src/main/kotlin/models/feecalculators/MallFeeCalculator.kt
+++ src/main/kotlin/models/feecalculators/MallFeeCalculator.kt
@@ -6,8 +6,16 @@ import models.feemodels.HourlyFeeModel
 
 class MallFeeCalculator : FeeCalculator {
     override fun getFinalPrice(duration: Long, vehicleType: VehicleType): Long {
-        val feePerHour = MallFactory.getRate(vehicleType)
+        val feePerHour = getRate(vehicleType)
         val feeModel : FeeModel = HourlyFeeModel()
         return feeModel.calculateFee(duration, feePerHour)
+    }
+
+    fun getRate(vehicleType: VehicleType): Long {
+        return when (vehicleType) {
+            VehicleType.CAR -> 20L
+            VehicleType.TWO_WHEELER -> 10L
+            VehicleType.HEAVY_VEHICLE -> 50L
+        }
     }
 }
--- src/main/kotlin/models/feecalculators/StadiumFeeCalculator.kt
+++ src/main/kotlin/models/feecalculators/StadiumFeeCalculator.kt
@@ -8,8 +8,8 @@ import java.lang.Long.min
 
 class StadiumFeeCalculator : FeeCalculator {
     override fun getFinalPrice(duration: Long, vehicleType: VehicleType): Long {
-        val intervals = StadiumFactory.getIntervals(vehicleType)
-        val rates = StadiumFactory.getRates(vehicleType)
+        val intervals = getIntervals(vehicleType)
+        val rates = getRates(vehicleType)
         var feeModel: FeeModel = FlatFeeModel()
 
         var finalPrice = 0L
@@ -24,5 +24,21 @@ class StadiumFeeCalculator : FeeCalculator {
         }
 
         return finalPrice
+    }
+
+    fun getIntervals(vehicleType: VehicleType): List<Interval> {
+        return when (vehicleType) {
+            VehicleType.TWO_WHEELER -> listOf(Interval(0L, 4L), Interval(4L, 12L), Interval(12L, Long.MAX_VALUE))
+            VehicleType.CAR -> listOf(Interval(0L, 4L), Interval(4L, 12L), Interval(12L, Long.MAX_VALUE))
+            VehicleType.HEAVY_VEHICLE -> listOf(Interval(0L, 0L))
+        }
+    }
+
+    fun getRates(vehicleType: VehicleType): List<Long> {
+        return when (vehicleType) {
+            VehicleType.TWO_WHEELER -> listOf(30L, 60L, 100L)
+            VehicleType.CAR -> listOf(60L, 120L, 200L)
+            VehicleType.HEAVY_VEHICLE -> listOf(0L)
+        }
     }
 }
